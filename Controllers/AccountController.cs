b472fe6cbbc4d8c0f88572c5d46031192f463246
Annemsucharitha <sucharithaannem@gmail.com>
2024-02-25 16:09:59 +0530

    first

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankLoginPageUsingDocker.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+             public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+
+            return RedirectToAction("Login");
+        }
+    }
+}
+
+
